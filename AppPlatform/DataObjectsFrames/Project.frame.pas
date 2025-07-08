12ca379000cfc4ae04c2bcf329c69d63a3925865
Kees Vermeulen <kees.vermeulen@a-dato.net>
2025-07-08 12:00:51 +0200

    Commit

---
+++ AppPlatform/DataObjectsFrames/Project.frame.pas
@@ -0,0 +1,81 @@
+﻿unit Project.frame;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, 
+  FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.Layouts, FMX.DataControl.ScrollableControl,
+  FMX.DataControl.ScrollableRowControl, FMX.DataControl.Static,
+  FMX.DataControl.Editable, FMX.DataControl.Impl, FMX.Edit, FMX.TabControl,
+  System.Actions, FMX.ActnList, LynxX.Controls.EditorPanel,
+  App.EditorManager.intf, FMX.ListBox, FMX.DataControl.Events;
+
+type
+  TProjectFrame = class(TFrame, IEditorManager)
+    IProject_Model: TDataControl;
+    Layout1: TLayout;
+    Label1: TLabel;
+    IProject_Name_1: TLabel;
+    Splitter1: TSplitter;
+    TabPages: TTabControl;
+    tbCustomfields: TTabItem;
+    tbGeneral: TTabItem;
+    Label2: TLabel;
+    IProject_Name_2: TEdit;
+    Layout2: TLayout;
+    Button1: TButton;
+    ActionList1: TActionList;
+    acOk: TAction;
+    Label3: TLabel;
+    IProject_Age_1: TEdit;
+    Label4: TLabel;
+    IProject_Customer_0: TComboBox;
+    Label5: TLabel;
+    IProject_Customer_Address_0: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    IProject_SFAccount_0: TComboBox;
+    procedure acOkExecute(Sender: TObject);
+    procedure IProject_ModelCellFormatting(const Sender: TObject; e:
+        DCCellFormattingEventArgs);
+  private
+    _EditorManager: IEditorManager;
+  public
+    constructor Create(AOwner: TComponent); override;
+
+    property EditorManager: IEditorManager read _EditorManager implements IEditorManager;
+  end;
+
+implementation
+
+uses
+  App.EditorManager.impl, System_;
+
+{$R *.fmx}
+
+procedure TProjectFrame.acOkExecute(Sender: TObject);
+begin
+  // Close;
+end;
+
+constructor TProjectFrame.Create(AOwner: TComponent);
+begin
+  inherited;
+  // _EditorManager := TEditorManager.Create(lyEditors);
+end;
+
+procedure TProjectFrame.IProject_ModelCellFormatting(const Sender: TObject; e: DCCellFormattingEventArgs);
+begin
+//  var js_obj: JSObjectReference;
+//
+//  if e.Value.TryGetValue<JSObjectReference>(js_obj) then
+//  begin
+//    e.Value := js_obj.Invoke<CString>('Name');
+//    e.FormattingApplied := True;
+//  end;
+end;
+
+end.
+
